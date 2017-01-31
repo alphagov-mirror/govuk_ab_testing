165c574a457d3eeedad20795962a21e4f6f0153b
Tijmen Brommet <tijmen@gmail.com>
2017-01-31 16:43:43 +0000

    Delegate methods so that test helpers can use them

--- lib/govuk_ab_testing/experiment.rb
+++ lib/govuk_ab_testing/experiment.rb
@@ -2,6 +2,8 @@ module GovukAbTesting
   class Experiment
     attr_reader :ab_test, :request
 
+    delegate :request_header, :cookie_name, :response_header, to: :ab_test
+
     # @param experiment_name [String] Lowercase experiment name, like `example`
     # @param request [ApplicationController::Request] the `request` in the
     # controller.
@@ -14,21 +16,21 @@ module GovukAbTesting
     #
     # @return [String] the current variant, "A" or "B"
     def variant_name
-      request.headers[ab_test.request_header] == "B" ? "B" : "A"
+      request.headers[request_header] == "B" ? "B" : "A"
     end
 
     # Configure the response
     #
     # @param [ApplicationController::Response] the `response` in the controller
     def add_response_header(response)
-      response.headers['Vary'] = ab_test.response_header
+      response.headers['Vary'] = response_header
     end
 
     # HTML meta tag used to track the results of your experiment
     #
     # @return [String]
     def analytics_meta_tag
-      '<meta name="govuk:ab-test" content="' + ab_test.cookie_name + ':' + variant_name + '">'
+      '<meta name="govuk:ab-test" content="' + cookie_name + ':' + variant_name + '">'
     end
   end
 end
