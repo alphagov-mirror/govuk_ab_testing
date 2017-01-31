9d58dc1b842b891a89385717a6ff9caacd0b43d2
Tijmen Brommet <tijmen@gmail.com>
2017-01-31 16:38:00 +0000

    Require the helpers

--- lib/govuk_ab_testing.rb
+++ lib/govuk_ab_testing.rb
@@ -1,6 +1,7 @@
 require "govuk_ab_testing/version"
 require "govuk_ab_testing/experiment"
 require "govuk_ab_testing/ab_test"
+require "govuk_ab_testing/minitest_helpers"
 
 module GovukAbTesting
 end
