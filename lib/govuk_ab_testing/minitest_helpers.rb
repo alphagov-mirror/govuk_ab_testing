799c88665615799742178c8e7a48cd6a0c76bb74
Tijmen Brommet <tijmen@gmail.com>
2017-02-02 16:45:52 +0000

    Move the A/B assertions into the test helper
    This is nicer because you can't forget to test it.

--- lib/govuk_ab_testing/minitest_helpers.rb
+++ lib/govuk_ab_testing/minitest_helpers.rb
@@ -8,16 +8,14 @@ module GovukAbTesting
       previous_variant = @request.headers[ab_test.request_header]
       @request.headers[ab_test.request_header] = variant
 
+      requested_variant = ab_test.requested_variant(@request)
+
       yield
 
+      assert_equal requested_variant.response_header, response.headers['Vary'], "You probably forgot to use `configure_response`"
+      assert_meta_tag "govuk:ab-test", requested_variant.meta_tag_name + ':' + requested_variant.variant_name, "You probably forgot to add the `analytics_meta_tag`"
+
       @request.headers[ab_test.request_header] = previous_variant
-    end
-
-    def assert_ab_test_rendered(ab_test_name)
-      requested_variant = GovukAbTesting::RequestedVariant.new(ab_test_name, @request)
-
-      assert_equal requested_variant.response_header, response.headers['Vary']
-      assert_meta_tag "govuk:ab-test", requested_variant.meta_tag_name + ':' + requested_variant.variant_name
     end
 
   private
