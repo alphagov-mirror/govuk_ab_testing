73bbfabf7311663364cad04539d608b26e21b76d
Tijmen Brommet <tijmen@gmail.com>
2017-02-02 16:56:34 +0000

    Add a test for the minitest helpers

--- lib/govuk_ab_testing/minitest_helpers.rb
+++ lib/govuk_ab_testing/minitest_helpers.rb
@@ -3,7 +3,7 @@ module GovukAbTesting
     def with_variant(args)
       ab_test_name, variant = args.first
 
-      ab_test = GovukAbTesting::AbTest.new(ab_test_name)
+      ab_test = GovukAbTesting::AbTest.new(ab_test_name.to_s)
 
       previous_variant = @request.headers[ab_test.request_header]
       @request.headers[ab_test.request_header] = variant
@@ -12,16 +12,16 @@ module GovukAbTesting
 
       yield
 
-      assert_equal requested_variant.response_header, response.headers['Vary'], "You probably forgot to use `configure_response`"
-      assert_meta_tag "govuk:ab-test", requested_variant.meta_tag_name + ':' + requested_variant.variant_name, "You probably forgot to add the `analytics_meta_tag`"
+      assert_equal ab_test.response_header, response.headers['Vary'], "You probably forgot to use `configure_response`"
+      assert_meta_tag "govuk:ab-test", ab_test.meta_tag_name + ':' + requested_variant.variant_name, "You probably forgot to add the `analytics_meta_tag`"
 
       @request.headers[ab_test.request_header] = previous_variant
     end
 
   private
 
-    def assert_meta_tag(name, content)
-      assert_select "meta[name='#{name}'][content='#{content}']"
+    def assert_meta_tag(name, content, message)
+      assert_select "meta[name='#{name}'][content='#{content}']", message
     end
   end
 end
