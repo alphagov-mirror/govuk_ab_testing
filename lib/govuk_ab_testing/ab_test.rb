7e914a2401d38e6c7e4ec952f02ed2813d691750
Alec Gibson <aleckgibson@googlemail.com>
2017-02-03 10:04:22 +0000

    Add AB Testing block

--- lib/govuk_ab_testing/ab_test.rb
+++ lib/govuk_ab_testing/ab_test.rb
@@ -25,5 +25,15 @@ module GovukAbTesting
     def meta_tag_name
       ab_test_name.capitalize
     end
+
+    def self.with_ab_testing(ab_test_name, request)
+      ab_test_name = ab_test_name.to_s
+      ab_test = AbTest.new(ab_test_name)
+      puts "Inside block"
+      puts local_variables
+      puts request
+      requested_variant = ab_test.requested_variant request
+      requested_variant.configure_response yield
+    end
   end
 end
